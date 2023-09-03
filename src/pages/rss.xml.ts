636fc6e054cd4502311b8c350fe4249bd02a438f
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2023-09-03 12:38:52 +0200

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,20 @@
-import rss from '@astrojs/rss'
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export const get = () => rss({
-
-    title: 'Tiny Rocket',
-    description: 'Tiny Rocket’s ExampleBlog',
-    
-    site: import.meta.env.SITE,
-    items: import.meta.glob('./blog/**/*.mdx'),
-  })
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'TinyRocket',
+    description: 'A humble Astronaut’s guide to the stars',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: post.data.customData,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
