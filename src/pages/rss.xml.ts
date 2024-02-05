d14db2c6a1478a891327fa436e2f69740842bdb4
Delgado Matías <mmatidelga2@gmail.com>
2024-02-05 13:03:23 -0300

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,13 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE } from "../config";
+import getSortedPosts from "../utils/getSortedPosts";
 
 export async function GET(context) {
+export async function GET() {
+  const posts = await getCollection("blog");
+  const sortedPosts = getSortedPosts(posts);
   return rss({
     title: 'Tiny Rocket',
     description: 'A humble Astronaut’s guide to the stars',
@@ -8,5 +15,14 @@ export async function GET(context) {
     items: await pagesGlobToRssItems(
       import.meta.glob('./blog/*.{md,mdx}'),
     ),
+    title: SITE.title,
+    description: SITE.description,
+    site: SITE.website,
+    items: sortedPosts.map(({ data, slug }) => ({
+      link: `posts/${slug}`,
+      title: data.title,
+      description: data.description,
+      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+    })),
   });
 }
