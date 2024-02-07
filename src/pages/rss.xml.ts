e1256803c9ad09147ebfb0f0f6e67473a19d3723
Delgado Matías <mmatidelga2@gmail.com>
2024-02-07 14:28:33 -0300

    FIX rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,14 +2,13 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import getSortedPosts from "../utils/getSortedPosts";
 
-export async function GET(context) {
 export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
   return rss({
-    title: 'Tiny Rocket',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: context.site,
+    title: "Tiny Rocket",
+    description: "A humble Astronaut’s guide to the stars",
+    site: "https://tinyrocket.pages.dev/",
     items: sortedPosts.map(({ data, slug }) => ({
       link: `posts/${slug}`,
       title: data.title,
